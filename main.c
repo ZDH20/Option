f4fb9cca084261adb3b66ad9610f33458bfa9be4
= <=>
2022-09-17 19:12:05 -0500

    Added tests.

--- main.c
+++ main.c
@@ -1,22 +1,46 @@
 #include "option.h"
+#include <assert.h>
 #include <stdio.h>
+
+test() {
+  Option op;
+
+  option_nonify(&op, INT);
+  option_somify(&op, WRAPINT(999));
+
+  UNWRAP_SECTION;
+  int x = UNWRAP(option_raw(&op), option_type(&op));
+  assert(x == 999);
+  UNWRAP_SECTION_END;
+
+  int x = 998;
+
+  option_somify(&op, WRAPVAR(x));
+
+  UNWRAP_SECTION;
+  int y = UNWRAP(option_raw(&op), option_type(&op));
+  assert(y == 998);
+  UNWRAP_SECTION_END;
+
+  option_nonify(&op, CHAR);
+  option_somify(&op, WRAPCHAR('z'));
+  UNWRAP_SECTION;
+  char y = UNWRAP(option_raw(&op), option_type(&op));
+  assert(y == 'z');
+  UNWRAP_SECTION_END;
+
+  char c = 'e';
+  option_nonify(&op, CHAR);
+  option_somify(&op, WRAPVAR(c));
+  UNWRAP_SECTION;
+  char y = UNWRAP(option_raw(&op), option_type(&op));
+  assert(y == 'e');
+  UNWRAP_SECTION_END;
+}
 
 int main(void) {
 
-  char a = 'j';
-
-  Option idx;
-  option_nonify(&idx, CHAR);
-  option_somify(&idx, WRAPVAR(a));
-
-  UNWRAP_SECTION;
-  char c = UNWRAP(option_raw(&idx), option_type(&idx));
-  printf("%c\n", c);
-  UNWRAP_SECTION_END;
-
-  option_nonify(&idx, INT);
-  option_somify(&idx, WRAPINT(5));
-  printf("%d\n", UNWRAP(option_raw(&idx), option_type(&idx)));
+  test();
 
   return 0;
 }
